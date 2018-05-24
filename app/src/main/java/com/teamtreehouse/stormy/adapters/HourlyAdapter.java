4a99255d4b224337a420156f14cd38b9b31ef1bf
kenwalger <kenalger@comcast.net>
2018-05-24 09:29:50 -0700

    S3v3

--- app/src/main/java/com/teamtreehouse/stormy/adapters/HourlyAdapter.java
+++ app/src/main/java/com/teamtreehouse/stormy/adapters/HourlyAdapter.java
@@ -1,26 +1,50 @@
 package com.teamtreehouse.stormy.adapters;
 
+import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
+import com.teamtreehouse.stormy.R;
+import com.teamtreehouse.stormy.Weather.Hour;
 import com.teamtreehouse.stormy.databinding.HourlyListItemBinding;
 
+import java.util.List;
+
 public class HourlyAdapter extends RecyclerView.Adapter<HourlyAdapter.ViewHolder> {
+
+  private List<Hour> hours;
+  private Context context;
+
+  public HourlyAdapter(List<Hour> hours, Context context) {
+    this.hours = hours;
+    this.context = context;
+  }
+
   @NonNull
   @Override
-  public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-    return null;
+  public HourlyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    HourlyListItemBinding binding = DataBindingUtil
+        .inflate(LayoutInflater.from(parent.getContext()),
+        R.layout.hourly_list_item,
+        parent,
+        false);
+
+    return new ViewHolder(binding);
   }
 
   @Override
-  public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+  public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    Hour hour = hours.get(position);
+    holder.hourlyListItemBinding.setHour(hour);
 
   }
 
   @Override
   public int getItemCount() {
-    return 0;
+    return hours.size();
   }
 
   public class ViewHolder extends RecyclerView.ViewHolder {
