2654d410898b430b1c04f10f2bea9ad1e0436da3
kenwalger <kenalger@comcast.net>
2018-05-24 08:02:08 -0700

    Starter Project Files

---
+++ README.md
@@ -0,0 +1,6 @@
+## Android Lists and Adapters
+
+This is the repository for the Android Lists and Adapters course on Treehouse
+
+Teacher: Ken W. Alger, [@kenwalger](https://www.twitter.com/kenwalger)
+
---
+++ app/src/main/java/com/teamtreehouse/stormy/MainActivity.java
@@ -0,0 +1,173 @@
+package com.teamtreehouse.stormy;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.teamtreehouse.stormy.databinding.ActivityMainBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.internal.tls.OkHostnameVerifier;
+
+public class MainActivity extends AppCompatActivity {
+
+  public static final String TAG = MainActivity.class.getSimpleName();
+  private CurrentWeather currentWeather;
+  private ImageView iconImageView;
+
+  double latitude = 37.8267;
+  double longitude = -122.4233;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    getForecast(latitude, longitude);
+    Log.d(TAG, "Main UI code is running, hooray!");
+  }
+
+  private void getForecast(double latitude, double longitude) {
+    final ActivityMainBinding binding = DataBindingUtil
+        .setContentView(MainActivity.this, R.layout.activity_main);
+
+    iconImageView = findViewById(R.id.iconImageView);
+
+    // Setup Dark Sky Link
+    TextView darkSky = findViewById(R.id.darkSkyAttribution);
+    darkSky.setMovementMethod(LinkMovementMethod.getInstance());
+
+    String apiKey = "57eaf3aa961968bf65b0619680588073";
+
+
+    String forecastURL = "https://api.darksky.net/forecast/" + apiKey +
+        "/" + latitude + "," + longitude;
+
+    if (isNetworkAvailable()) {
+      OkHttpClient client = new OkHttpClient();
+
+      Request request = new Request.Builder()
+          .url(forecastURL)
+          .build();
+
+      Call call = client.newCall(request);
+      call.enqueue(new Callback() {
+        @Override
+        public void onFailure(Call call, IOException e) {
+
+        }
+
+        @Override
+        public void onResponse(Call call, Response response) throws IOException {
+          try {
+            String jsonData = response.body().string();
+            Log.v(TAG, jsonData);
+            if (response.isSuccessful()) {
+              currentWeather = getCurrentDetails(jsonData);
+
+              final CurrentWeather displayWeather = new CurrentWeather(
+                  currentWeather.getLocationLabel(),
+                  currentWeather.getIcon(),
+                  currentWeather.getTime(),
+                  currentWeather.getTemperature(),
+                  currentWeather.getHumidity(),
+                  currentWeather.getPrecipChance(),
+                  currentWeather.getSummary(),
+                  currentWeather.getTimeZone()
+              );
+
+              binding.setWeather(displayWeather);
+
+              runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                  Drawable drawable = getResources().getDrawable(displayWeather.getIconId());
+                  iconImageView.setImageDrawable(drawable);
+                }
+              });
+
+            } else {
+              alertUserAboutError();
+            }
+          } catch (IOException e) {
+            Log.e(TAG, "IO Exception caught: ", e);
+          } catch (JSONException e) {
+            Log.e(TAG, "JSON Exception caught: ", e);
+          }
+
+        }
+      });
+    }
+    else {
+      Toast.makeText(this, R.string.network_unavailable_message,
+          Toast.LENGTH_LONG).show();
+    }
+  }
+
+  private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
+    JSONObject forecast = new JSONObject(jsonData);
+
+    String timezone = forecast.getString("timezone");
+    Log.i(TAG, "From JSON: " + timezone);
+
+    JSONObject currently = forecast.getJSONObject("currently");
+
+    CurrentWeather currentWeather = new CurrentWeather();
+
+    // Parse weather data from currently object
+    currentWeather.setHumidity(currently.getDouble("humidity"));
+    currentWeather.setTime(currently.getLong("time"));
+    currentWeather.setIcon(currently.getString("icon"));
+    currentWeather.setLocationLabel("Alcatraz Island, CA");
+    currentWeather.setPrecipChance(currently.getDouble("precipProbability"));
+    currentWeather.setSummary(currently.getString("summary"));
+    currentWeather.setTemperature(currently.getDouble("temperature"));
+    currentWeather.setTimeZone(timezone);
+
+    Log.d(TAG, currentWeather.getFormattedTime());
+
+    return currentWeather;
+  }
+
+  private boolean isNetworkAvailable() {
+    ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+    NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+    boolean isAvailable = false;
+
+    if (networkInfo != null && networkInfo.isConnected()) {
+      isAvailable = true;
+    }
+
+    return isAvailable;
+  }
+
+  private void alertUserAboutError() {
+    AlertDialogFragment dialog = new AlertDialogFragment();
+    dialog.show(getFragmentManager(), "error_dialog");
+  }
+
+  public void refreshOnClick(View view) {
+    getForecast(latitude, longitude);
+    Toast.makeText(this, "Refreshing data", Toast.LENGTH_LONG).show();
+  }
+
+}
